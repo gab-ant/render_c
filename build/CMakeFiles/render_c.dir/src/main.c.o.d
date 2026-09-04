e0aa49de3559df4e67fb75ec6783e3c97b52bc06
gab-ant <gabrielantonsson.ga@gmail.com>
2026-09-04 17:33:36 +0200

    Opening and coloring window

--- build/CMakeFiles/render_c.dir/src/main.c.o.d
+++ build/CMakeFiles/render_c.dir/src/main.c.o.d
@@ -1,7 +1,10 @@
 CMakeFiles/render_c.dir/src/main.c.o: \
  /home/gabant/work/programming/projects/render_c/src/main.c \
  /usr/include/stdc-predef.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_init.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_stdinc.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_platform_defines.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h /usr/include/string.h \
  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
  /usr/include/features.h /usr/include/features-time64.h \
  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
@@ -10,10 +13,92 @@ CMakeFiles/render_c.dir/src/main.c.o: \
  /usr/include/x86_64-linux-gnu/bits/long-double.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/include/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/types/wint_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_begin_code.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
  /usr/include/x86_64-linux-gnu/bits/types.h \
  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
  /usr/include/x86_64-linux-gnu/bits/time64.h \
- /usr/include/x86_64-linux-gnu/bits/wchar.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
- /usr/include/x86_64-linux-gnu/bits/stdint-least.h
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /usr/include/inttypes.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdbool.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_close_code.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_error.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_events.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_audio.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_endian.h \
+ /usr/include/endian.h /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_mutex.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_atomic.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_thread.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_properties.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_iostream.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_camera.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_pixels.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_surface.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_blendmode.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_rect.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_gamepad.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_guid.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_joystick.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_power.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_sensor.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_keyboard.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_keycode.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_scancode.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_video.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_mouse.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_notification.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_pen.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_touch.h \
+ /usr/include/stdio.h /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_assert.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_asyncio.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_bits.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_clipboard.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_cpuinfo.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_dialog.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_dlopennote.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_filesystem.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_gpu.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_haptic.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_hidapi.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_hints.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_loadso.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_locale.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_log.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_messagebox.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_metal.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_misc.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_platform.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_process.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_render.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_storage.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_system.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_time.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_timer.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_tray.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_version.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_oldnames.h \
+ /home/gabant/work/programming/projects/render_c/vendored/SDL/include/SDL3/SDL_main.h
--- src/main.c
+++ src/main.c
@@ -1,4 +1,10 @@
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_pixels.h"
+#include "SDL3/SDL_video.h"
+#include <stdio.h>
 #include <stdint.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 typedef int8_t i8;
 typedef int16_t i16;
@@ -13,5 +19,44 @@ typedef float f32;
 typedef double f64;
 
 int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window;
+    bool done = false;
+
+    window = SDL_CreateWindow("My render", 100, 100, SDL_WINDOW_RESIZABLE);  
+    
+    if (window == NULL) {
+	SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+	return 1;
+    }
+    
+    // Declaring colors (mapping RGB values to a u32)
+    const SDL_PixelFormatDetails* FmtDetails = SDL_GetPixelFormatDetails(SDL_GetWindowSurface(window)->format);
+
+    const u32 RedColor = SDL_MapRGB(
+	    FmtDetails,
+	    NULL,
+	    255,
+	    0,
+	    0
+    );
+    
+    // Make screen red
+    SDL_FillSurfaceRect(SDL_GetWindowSurface(window), NULL, RedColor);
+    SDL_UpdateWindowSurface(window);
+
+    while (!done) {
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event)) {
+	    if (event.type == SDL_EVENT_QUIT) {
+		done = true;
+	    }
+	}
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
